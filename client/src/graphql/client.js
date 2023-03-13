7be73ee3d81f1ea7c988512e4edb83118d8e9fd2
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-03-13 17:29:40 +0200

    add authorization to chat

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -3,14 +3,20 @@ import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { createClient as createWsClient } from "graphql-ws";
 import { Kind, OperationTypeNode } from "graphql";
+import { getAccessToken } from "../auth";
 
 const GRAPHQL_HTTP_URL = "http://localhost:9000/graphql";
-const GRAPHQL_WS_URL = "http://localhost:9000/graphql";
+const GRAPHQL_WS_URL = "ws://localhost:9000/graphql";
 
 const httpLink = new HttpLink({
   uri: GRAPHQL_HTTP_URL,
 });
-const wsLink = new GraphQLWsLink(createWsClient({ url: GRAPHQL_WS_URL }));
+const wsLink = new GraphQLWsLink(
+  createWsClient({
+    url: GRAPHQL_WS_URL,
+    connectionParams: () => ({ accessToken: getAccessToken() }),
+  })
+);
 
 function isSubscription({ query }) {
   const defination = getMainDefinition(query);
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const MESSAGES_QUERY = gql`
   query MessagesQuery {
@@ -13,6 +13,16 @@ export const MESSAGES_QUERY = gql`
 export const ADD_MESSAGE_MUTATION = gql`
   mutation AddMessageMutation($input: MessageInput!) {
     message: addMessage(input: $input) {
+      id
+      from
+      text
+    }
+  }
+`;
+
+export const MESSAGE_ADDED_SUBSCRIPTION = gql`
+  subscription MessageAddedSubscription {
+    message: messageAdded {
       id
       from
       text
